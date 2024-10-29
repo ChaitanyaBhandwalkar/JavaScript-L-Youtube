eb643587d3b3e603cdac75b4003aa75e81ceec8a
ChaitanyaBhandwalkar <110159714+ChaitanyaBhandwalkar@users.noreply.github.com>
2024-10-29 13:01:21 +0000

    DataTypes

---
+++ 01_Basics/02_DataType.js
@@ -0,0 +1,23 @@
+"use Strict";  //Treat All JS Code AS A Newer Version Or JS.
+
+// alert(3+3);  // We Are USing NodeJS, Not A Browser
+// console.log(alert);
+
+console.log(3 + 3);
+
+console.log("Chaitanya");
+
+
+let name = 'Chaitanya' ; // String
+let age = 23;
+let isLoggedIn = false;
+
+// console.log("CB");
+// console.log(23);
+// console.log(false);
+
+console.log(typeof null);
+
+
+
+
