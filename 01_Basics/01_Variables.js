fedbeda212b35e337fa4e5a697e7d5e31f018c7d
ChaitanyaBhandwalkar <110159714+ChaitanyaBhandwalkar@users.noreply.github.com>
2024-10-29 13:00:11 +0000

    Variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,20 @@
+const accountId = 1223345
+let accountEmail = "chaitanya@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+let accountState = "maharashtra";
+
+// accountId = 2 // Not Allowed
+console.log(accountId);
+
+accountEmail = "CB@gmail.com"
+// accountId = "121212"
+accountPassword = "03020302"
+accountCity = "Satara"
+
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
+
+
+
+console.log(accountId);
