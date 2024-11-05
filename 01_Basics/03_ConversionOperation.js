70cda0a5276723cb5c59e44e5c3fafa89b0ea124
ChaitanyaBhandwalkar <110159714+ChaitanyaBhandwalkar@users.noreply.github.com>
2024-11-05 11:56:48 +0530

    Update 03_ConversionOperation.js

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -18,3 +18,10 @@ console.log(booleanIsLoggedIn);
 let value = 3;
 let negValue = -value;
 console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*3);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
